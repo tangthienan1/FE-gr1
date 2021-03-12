bff49d11e5b9f32b606e4122315217de020288d2
nhatlam1995 <43503909+nhatlam1995@users.noreply.github.com>
2021-03-13 02:16:36 +0700

    [FE][lamnngcs18047] Homepage
    Homepage UI includes:
    _ Navbar
    _ Banner carousel
    _ Search bar
    _ Map content with card view
    _ Gallery carousel
    _ Footer

---
+++ src/components/Base/include/Footer/Footer.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className="main-footer">
+            <div className="container">
+                <div className="row align-items-start">
+                    <div className="col-sm text-left mx-auto">
+                        <h4>HÀ NỘI</h4>
+                        <hr />
+                        <ul className="list-unstyled">
+                            <li>ĐẠI HỌC GREENWICH (VIỆT NAM)</li>
+                            <li>Tòa nhà DETECH -Số 8 Tôn Thất Thuyết-P.Mỹ Đình 2-Q.Nam Từ Liêm</li>
+                            <li>Điện thoại: 024.7300.2266</li>
+                            <li>Hotline: 0981.558.080 | 0971.274.545</li>
+                        </ul>
+                    </div>
+                    <div className="col-sm text-left mx-auto">
+                        <h4>Hồ Chí Minh</h4>
+                        <hr />
+                        <ul className="list-unstyled">
+                            <li>ĐẠI HỌC GREENWICH (VIỆT NAM)</li>
+                            <li>CS1: Số 142-146 Phạm Phú Thứ - Phường 4 - Quận 6 (Cuối đường 3/2)</li>
+                            <li>CS2: 205 Nguyễn Xí, Phường 26, Bình Thạnh.</li>
+                            <li>Điện thoại: 028.7300.2266</li>
+                            <li>Hotline: 0933.108.554 | 0971.294.545</li>
+                        </ul>
+                    </div>
+                    <div className="col-sm text-left mx-auto">
+                        <h4>ĐÀ NẴNG</h4>
+                        <hr />
+                        <ul className="list-unstyled">
+                            <li>ĐẠI HỌC GREENWICH (VIỆT NAM)</li>
+                            <li>658 Ngô Quyền, Phường An Hải Bắc, Quận Sơn Trà, Tp. Đà Nẵng</li>
+                            <li>Điện thoại: 0236.730.2266</li>
+                            <li>Hotline: 0934.892.687</li>
+                        </ul>
+                    </div>
+                    <div className="col-sm text-left mx-auto">
+                        <h4>CẦN THƠ</h4>
+                        <hr />
+                        <ul className="list-unstyled">
+                            <li>ĐẠI HỌC GREENWICH (VIỆT NAM)</li>
+                            <li>Số 160 đường 30/4, phường An phú, quận Ninh Kiều - TP. Cần Thơ</li>
+                            <li>Điện thoại: 0292.3512.369</li>
+                            <li>Hotline: 0968.670.804 | 0936.600.861</li>
+                        </ul>
+                    </div>
+                </div>
+                <hr />
+                <div className="footer-bottom">
+                    <p className="col-sm text-left mx-auto">
+                        &copy;{new Date().getFullYear()} CMS Greenwich Magazine System - All Rights Reserved
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Base/include/Header/Header.jsx
@@ -0,0 +1,67 @@
+import React, { useState, Component } from 'react';
+import { UncontrolledCarousel } from 'reactstrap';
+import Banner from '../Banner/Banner';
+import Media from 'react-media'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    NavbarText,
+    Col,
+} from 'reactstrap';
+import {
+    Form,
+    FormControl,
+    Button,
+
+} from 'react-bootstrap';
+
+const Header = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <div>
+            <Navbar light expand="md">
+                <NavbarBrand href="/" >Greenwich University Magazine System</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="me-auto" navbar>
+                        <Col xs="auto">
+                            <NavItem>
+                                <NavLink href="/">Guest</NavLink>
+                            </NavItem>
+                        </Col>
+                        <Col xs="auto">
+                            <NavItem>
+                                <NavLink href="/dashboard">Dashboard</NavLink>
+                            </NavItem>
+                        </Col>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+            <Media query='(min-width: 547px)'>
+                {matches => {
+                    return matches ?
+                        <>
+                            <div className=".d-sm-none .d-md-block">
+                                <Banner />
+                                <Form inline>
+                                    <FormControl type="text" placeholder="Search" className=" mr-sm-2" />
+                                    <Button type="submit">Submit</Button>
+                                </Form>
+                            </div>
+                        </>
+                        : null
+                }}
+            </Media>
+        </div >
+    );
+}
+
+export default Header;
