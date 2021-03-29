cbb798cf234168f0f52d53d673976830ed5f0ce7
nhatlam1995 <43503909+nhatlam1995@users.noreply.github.com>
2021-03-30 00:16:34 +0700

    [FE][lamnngcs18047] ContentAPI
    Te

--- src/api/APIurl.js
+++ src/api/APIurl.js
@@ -1,2 +1,4 @@
 // export constant API ex:/commentAPi
 export const LOGIN_API = '/authenticat/';
+export const USER_LIST = '';
+export const CONTRIBUTION_LIST = 'https://jsonplaceholder.typicode.com/photos?_start=0&_limit=60';
---
+++ src/api/contributionAPI.js
@@ -0,0 +1,13 @@
+
+import axiosClient from "../axiosClient";
+import { CONTRIBUTION_LIST } from "./APIurl";
+
+
+const contributionAPI = {
+    getContribution: () => {
+        const url = CONTRIBUTION_LIST;
+        return axiosClient.get(url);
+    }
+};
+
+export default contributionAPI;
