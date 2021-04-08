826b9c83b53681f3fa48068c33e65537f6d327e1
VuroNguyen <46697043+VuroNguyen@users.noreply.github.com>
2021-04-08 11:12:43 +0700

    [FE][Longnhgcs18171] Approved_Pending_Student

--- src/pages/DashBoard/Student/components/Pending.jsx
+++ src/pages/DashBoard/Student/components/Pending.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { Card, Button, CardTitle, CardText, Form, FormGroup, Label, Input, FormText, Col } from 'reactstrap';
+import SearchBar from '../../../../components/Base/include/Searchbar';
 
 function Pending() {
     return (
         <div>
-            <h3>Hello World!! from pendding tab</h3>
+            <div className='form_cb'>
+                <SearchBar/>
+                <FormGroup>
+                    <Input className='form-input' type="select" name="select">
+                        <option>Articles</option>
+                        <option>Photogragh</option>
+                    </Input>
+                </FormGroup>
+            </div>
+
+            <div>
+                <Card className='card'>
+                    <div className='card-text-top'>
+                        <CardText className='card-title'>This is title</CardText>
+                        <CardText className='card-date'>Date</CardText>
+                        <CardText className='card-status'>Approve/Submit</CardText>
+                    </div>
+                    <CardText className='text-center'>With supporting text below as a natural lead-in to additional content.</CardText>
+                </Card>
+
+                <Card className='card'>
+                    <div className='card-text-top'>
+                        <CardText className='card-title'>This is title</CardText>
+                        <CardText className='card-date'>Date</CardText>
+                        <CardText className='card-status'>Approve/Submit</CardText>
+                    </div>
+                    <CardText className='text-center'>With supporting text below as a natural lead-in to additional content.</CardText>
+                </Card>
+
+                <Card className='card'>
+                    <div className='card-text-top'>
+                        <CardText className='card-title'>This is title</CardText>
+                        <CardText className='card-date'>Date</CardText>
+                        <CardText className='card-status'>Approve/Submit</CardText>
+                    </div>
+                    <CardText className='text-center'>With supporting text below as a natural lead-in to additional content.</CardText>
+                </Card>
+            </div>
         </div>
     )
 }
