2b1eba8fa38cc74d03e9895d61fc03cd386f34b2
An Tang <tangthienan9@gmail.com>
2021-04-20 04:02:38 +0700

    homepage #1fix

--- src/pages/DashBoard/Admin/Modal/modal.js
+++ src/pages/DashBoard/Admin/Modal/modal.js
@@ -6,7 +6,6 @@ import '../admin.css'
 
 
 const Modal = ({ isShowing, hide }) => isShowing ? ReactDOM.createPortal(
-
   <React.Fragment>
     <div className="modal-overlay"/>
     <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
@@ -16,36 +15,11 @@ const Modal = ({ isShowing, hide }) => isShowing ? ReactDOM.createPortal(
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        {/* <div className="modal-header1">
-                <p>Create new users</p>
-                </div>
-            <div className="modal-content1">
-                <div className="modal-form1">
-                    <label htmlFor="ID">ID:</label>
-                    <input type="number" name="ID" id="ID"></input>
-                </div>
-                <div className="modal-form1">
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" id="name"></input>
-                </div>
-                <div className="modal-form1">
-                    <label htmlFor="falcuty">Falcuty:</label>
-                    <input type="text" name="falcuty" id="falcuty"></input>
-                </div>
-                <div className="modal-form1">
-                    <label htmlFor="role">Role:</label>
-                    <input type="text" name="role" id="role"></input>
-                </div>
-                <div className="modal-footer1">
-                    <button classname="btn-create">Create</button>
-                </div>
-            </div> */}
-             <p>
+        <p>
           Hello, I'm a modal.
         </p>
       </div>
     </div>
-  </React.Fragment>,document.body
+  </React.Fragment>, document.body
 ) : null;
-
 export default Modal;
